d59823d6e4e32c9b557be07317fe6546f258e708
Junsu97 <87612274+Junsu97@users.noreply.github.com>
2023-05-23 10:26:56 +0900

    Add files via upload
    .

---
+++ c_study/10-11.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void gugu(int dan){
+	int result;
+	for(int i=1; i < 10; i++){
+		result = dan * i;
+		printf("%d X %d = %d \n",dan,i,result);
+	}
+}
+
+void main(){
+	int dan;
+	printf("출력하고 싶은 단을 입력 : ");
+	scanf("%d", &dan);
+
+	gugu(dan);
+}
