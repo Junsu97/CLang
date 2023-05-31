33b83d848f5b66ffe058aa0c222613f0aa3b83e7
Junsu97 <87612274+Junsu97@users.noreply.github.com>
2023-06-01 08:42:40 +0900

    Add files via upload
    .

---
+++ c_study/12-8.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<malloc.h>
+
+void main(){
+	int* p;
+	int i, hap = 0;
+	int cnt;
+
+	printf(" 입력할 개수는? ");
+	scanf("%d", &cnt);
+
+	p = (int*) malloc(sizeof(int) * cnt);
+
+	for(i = 0; i < cnt; i++){
+		printf(" %d번째 숫자 : ", i+1);
+		scanf("%d",p+i);
+	}
+
+	for(i = 0; i < cnt; i++){
+
+		if(*(p+i)%2 != 0){
+			continue;
+
+		}else{
+			hap += *(p+i);
+		}
+	}
+
+	printf("입력 짝수 합 ==> %d\n", hap);
+
+	free(p);
+}
