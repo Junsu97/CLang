8253405e56aa616baa50ac46be2b825b1f3ac9f2
junsu97 <wnstn7759@naver.com>
2023-06-19 21:38:22 +0900

    기말고사

--- c_study/gimal.c
+++ c_study/gimal.c
@@ -1,33 +1,10 @@
 ﻿#include<stdio.h>
-#include<malloc.h>
-#include<string.h>
-void main(){
-	struct student{
-		char name[10];
-		int age;
-	};
 
-	struct student s;
-	struct student *p;
-	int count;
-	printf("입력할 학생 수 : ");
-	scanf("%d",&count);
+void main(int argc, int argv[]){
+	if(argc != 3){
+		printf("매개변수를 3개 입력해주세요.");
+		return;
+	}
+
 	
-	p = (struct student*)malloc(sizeof(s) * count);
-
-	for(int i = 0;  i < count; i++){
-		printf("이름과 나이 입력 : ");
-		scanf("%s %d",p[i].name, &p[i].age);
-	}
-
-	printf("\n\n-- 학생 명단 --\n");
-	for(int i = 0; i < count; i++){
-		printf("이름: %s , 나이:%d \n",p[i].name, p[i].age);
-	}
-
-	for(int i = 0; i < count; i++){
-		free(p+i);
-	}
-	
-		
 }
